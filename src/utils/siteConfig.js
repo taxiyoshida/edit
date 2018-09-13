6b333b28903b82f74795b337efb976f17240be5e
taxiyoshida <taxi.yoshida@gmail.com>
2018-09-13 15:03:45 +0900

    Initialize repository

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: 'edit',
+  siteTitleAlt: 'Knowledge for Next Humanity', // This allows an alternative site title for SEO schema.
+  publisher: 'Takushi Yoshida', // Organization name used for SEO schema
+  siteDescription:
+    'Tech Business news and analytics you need to get for evolve yoursef to next human',
+  siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  author: 'GCN User', // Author for RSS author segment and SEO schema
+  authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+  userTwitter: '@twitter', // Change for Twitter Cards
+  shortTitle: 'GCN App', // Used for App manifest e.g. Mobile Home Screen
+  shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
+  shareImageWidth: 900, // Change to the width of your default share image
+  shareImageHeight: 600, // Change to the height of your default share image
+  siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
+  backgroundColor: '#e9e9e9', // Used for Offline Manifest
+  themeColor: '#121212', // Used for Offline Manifest
+  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed
+}
