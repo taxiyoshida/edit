09532ee1042a94a1e99610fb481834c8fcbc210a
taxiyoshida <taxi.yoshida@gmail.com>
2019-02-04 12:45:42 +0900

    1

--- src/templates/page.js
+++ src/templates/page.js
@@ -30,11 +30,6 @@ export const query = graphql`
     contentfulPage(slug: { eq: $slug }) {
       title
       slug
-      metaDescription {
-        internal {
-          content
-        }
-      }
       body {
         childMarkdownRemark {
           html
